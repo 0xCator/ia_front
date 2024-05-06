3555d035bc101dbaef464ceeb4e00b3f329b2946
Nexusrex <ahmedtahoun55@gmail.com>
2024-05-06 22:43:21 +0300

    Refactor: Styling subcomponents

--- src/Components/MainDashboard/Dashboards/ProjectCard/ProjectCard.tsx
+++ src/Components/MainDashboard/Dashboards/ProjectCard/ProjectCard.tsx
@@ -1,10 +1,18 @@
 // ProjectCard.tsx
 
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, IconButton } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import PersonIcon from '@mui/icons-material/Person';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
 
 interface ProjectCardProps {
     projectName: string;
@@ -15,6 +23,8 @@ interface ProjectCardProps {
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ projectName, state, onShowSettings, onAssign, onDelete }) => {
+    const navigate = useNavigate();
+    
     const handleAcceptRequest = async () => {
         try {
             // Make POST request to backend API to accept request
@@ -50,38 +60,48 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ projectName, state, onShowSet
     switch (state) {
         case 0:
             cardContent = (
-                <OverlayTrigger
-                    placement="right"
-                    delay={{ show: 250, hide: 400 }}
-                    overlay={<Tooltip id="button-tooltip">Tab to display Project Settings</Tooltip>}
-                >
-                    <Card className="project-card red" onClick={() => onShowSettings(projectName)}>
-                        <Card.Body>
-                            <Card.Title>{projectName}</Card.Title>
-                        </Card.Body>
+                    <Card variant='outlined' sx={{ borderColor: 'blue', minWidth: 215, maxWidth: 215, minHeight: 113, maxHeight: 113 }}>
+                        <CardActionArea onClick={() => navigate(`/project/1`)}>
+                            <CardContent>
+                                <Typography variant="h5" component="div">
+                                    {projectName}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions sx={{justifyContent: "right"}}>
+                            <IconButton size="small" color="primary" onClick={() => onShowSettings(projectName)}><ManageAccountsIcon/></IconButton>
+                        </CardActions>
                     </Card>
-                </OverlayTrigger>
             );
             break;
         case 1:
             cardContent = (
-                <Card className="project-card green">
-                    <Card.Body>
-                        <Card.Title>{projectName}</Card.Title>
-                    </Card.Body>
+                <Card variant='outlined' sx={{ borderColor: 'green', minWidth: 215, maxWidth: 215, minHeight: 113, maxHeight: 113 }}>
+                    <CardActionArea onClick={() => navigate(`/project/1`)}>
+                        <CardContent>
+                            <Typography variant="h5" component="div">
+                                {projectName}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions sx={{justifyContent: "right"}}>
+                        <IconButton size="small" disabled><PersonIcon/></IconButton>
+                    </CardActions>
                 </Card>
             );
             break;
         case 2:
             cardContent = (
-                <Card className="project-card blue">
-                    <Card.Body>
-                        <Card.Title>{projectName}</Card.Title>
-                        <div className="project-card-buttons">
-                            <Button className="reject" onClick={handleRejectRequest}>Reject</Button>
-                            <Button className="accept" onClick={handleAcceptRequest}>Accept</Button>
-                        </div>
-                    </Card.Body>
+                <Card variant='outlined' sx={{ minWidth: 215, maxWidth: 215, minHeight: 113, maxHeight: 113 }}>
+                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            {projectName}
+                        </Typography>
+                    </CardContent>
+                    <CardActions sx={{justifyContent: "right"}}>
+                        <IconButton size="small" color="success" onClick={handleAcceptRequest}><CheckIcon/></IconButton>
+                        <IconButton size="small" color="error" onClick={handleRejectRequest}><CloseIcon/></IconButton>
+                    </CardActions>
                 </Card>
             );
             break;
