75f31f5644c21ad361a2fb4cefe5f7b9c37979ce
Nexusrex <ahmedtahoun55@gmail.com>
2024-05-10 00:19:57 +0300

    Refactor: Minor provider change

--- src/Socket/SocketProvider.tsx
+++ src/Socket/SocketProvider.tsx
@@ -11,12 +11,13 @@ export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({ childr
     const nameid = getUserData()?.user.nameid;
     const socket = useWebSocket(websocketPath(nameid!));
 
+    
     useEffect(() => {
         if (socket.readyState === 1 && !initialized) {
             console.log('Connected to WebSocket, ID is:', nameid);
             setInitialized(true);
         }
-    }, [socket.readyState, nameid, initialized]);
+    }, [nameid, initialized]);
 
     return (
         <WebSocketContext.Provider value={socket}>
